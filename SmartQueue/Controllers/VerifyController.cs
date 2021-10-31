39e7e2c8a6def159324ff0e9ca37e401e475bb32
Gerrit van Zyl <61610405+gerrit450@users.noreply.github.com>
2021-10-31 13:39:09 +1300

    This controller is used to route to the verification page if a unsuitable password is entered and not matched with the User database. If the correct authentication details are entered, the user will proceed

---
+++ SmartQueue/Controllers/VerifyController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartQueue.Controllers
+{
+    public class VerifyController : Controller
+    {
+        
+        // GET: Verify
+        public ActionResult Index()
+        {
+            // used to retrieve input from home via sent through POST request from homepage for authentication.
+            string us = Request.Form["username"]; 
+            string pw = Request.Form["password"];
+            ViewData["count"] = PatentModel.patentInfo.getTableCount();
+            authenticating(us,pw);
+            return View();
+        }
+
+        private void authenticating(string user, string pass)
+        {
+
+            if (authenticateModel.Members.readUser(user,pass) == true)
+            {
+                ViewData["auth"] = true;
+            }
+            else
+            {
+                ViewData["auth"] = false;
+            }
+        }
+    }
+}
