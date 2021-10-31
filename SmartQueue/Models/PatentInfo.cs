e3c461841dca53c26719c3def4a6cbe990b5bc61
Gerrit van Zyl <61610405+gerrit450@users.noreply.github.com>
2021-10-31 13:46:24 +1300

    All basic CRUD operations have been created. Now I will integrate this with the website and will design it that it will do CRUD based on firstname provided in the Verify page. Will likely have to implement an AJAX

--- SmartQueue/Models/PatentInfo.cs
+++ SmartQueue/Models/PatentInfo.cs
@@ -1,11 +1,16 @@
 ﻿using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
 using System;
+using System.Web.Mvc;
+using System.Collections.Generic;
 
 namespace PatentModel
 {
     public class patentInfo 
     {
+        List<string> itemList = new List<string>();
+
         public static string value;
         /*There are 3 ways of ASP.NET Modelling API's such as;
          * low-level Api
@@ -21,8 +26,8 @@ namespace PatentModel
             var item = table.GetItem("1", "1");
             value = item["PatentID"]; //the 
         }
-        
-        public static void insertValues()
+
+        public static void insertValues() // insert a new row into the table
         {
             var client = new AmazonDynamoDBClient();
             var table = Table.LoadTable(client, "PatentInfo");
@@ -36,11 +41,11 @@ namespace PatentModel
             table.PutItem(item); //insert a new row into the table.
         }
 
-        public static void updateRow()
+        public static void updateRow() //updating existing row.
         {
             var client = new AmazonDynamoDBClient();
             var table = Table.LoadTable(client, "PatentInfo");
-            var item = table.GetItem("01", "01");
+            var item = table.GetItem("1", "1");
             item["PatentID"] = "1";
             item["NameID"] = "1";
             item["FirstName"] = "Adam";
@@ -57,7 +62,26 @@ namespace PatentModel
             var item = table.DeleteItem("2", "2"); //specify which row to delete based on primary and hash key.
         }
         
-    }
+        public static int getTableCount()
+        {
+            int itemCount = 0;
+            var client = new AmazonDynamoDBClient();
+            var table = Table.LoadTable(client, "PatentInfo");
+            //var itemCount = table.GetItemAsync("2");
 
-    
+            var request = new ScanRequest
+            {
+                TableName = "PatentInfo"
+            };
+
+            var response = client.Scan(request);
+            var result = response.ScannedCount;
+
+            foreach (Dictionary<string, AttributeValue> item in response.Items)
+            {
+                itemCount = itemCount + 1;
+            }
+            return itemCount;
+        }
+    }
 }
