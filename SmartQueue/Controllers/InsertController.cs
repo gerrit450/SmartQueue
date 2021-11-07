b988f7a1fe6fb0009baa0fb6f2ca46ef03e4ac4f
Gerrit van Zyl <61610405+gerrit450@users.noreply.github.com>
2021-11-08 11:02:38 +1300

    controller that inserts a new row into the database. Later on, will do new attributes such as address

---
+++ SmartQueue/Controllers/InsertController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartQueue.Controllers
+{
+    public class InsertController : Controller
+    {
+        public ActionResult Index()
+        {
+            PatentModel.patentInfo.insertValues(Request.Form["FirstName"], Request.Form["LastName"]);
+            return View();
+        }
+    }
+}
